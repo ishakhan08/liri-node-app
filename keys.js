29b192456a0e39ca7f03ae56fe71df67c62af5e2
Isha Khan <khan.isha08@gmail.com>
2018-05-14 22:14:13 -0500

    final

---
+++ keys.js
@@ -0,0 +1,12 @@
+exports.twitterKeys = {
+  consumer_key: 'qj8IQopUMINkohc4m9t6OgJfu',
+  consumer_secret: 'S3LkFR9r9SLYmJw1yQop9Qw6lEhdjfColtjE1YoPcSIsxw5CWd',
+  access_token_key: '994638781444739077-p5IHpCWbWfSJc663Ps13mu2ocOv79ka',
+  access_token_secret: 'Asd2EVyQMzV2BQxbhXdWD98igiJurMZTcxDecF17On20k'  
+};
+
+exports.spotifyKeys = {
+  id: 'cff7d9dc05084018a44182b1a6fe77c2',
+  secret: 'eb29664ab0ce40ae967d55eb7c6a7d90'
+};
+
